b07933802f3aaa556c2b9fe3e8b0bc037d4e195b
majobodo <majobodo@web.de>
2025-02-12 18:27:51 +0100

    Remove debug print statements from offset calculation
    Debugging print statements in `apply_func.py` were removed to clean up the code. These included median, distances, and final offset outputs, making the function more suitable for production use. Logic for determining offsets remains intact.

--- pynpoint/util/apply_func.py
+++ pynpoint/util/apply_func.py
@@ -115,15 +115,9 @@ def align_image(image_in: np.ndarray,
     offset_array = np.array(all_offsets)
     offset_median = np.median(offset_array, axis=0)
 
-    print(f'Offset median: {offset_median}')
-    print(f'Offsets: {all_offsets}')
-
     # check for individual offsets that are too far from the median
     distances_x = np.abs(offset_array - offset_median)[:, 0]
     distances_y = np.abs(offset_array - offset_median)[:, 1]
-
-    print(f'Distances x: {distances_x}')
-    print(f'Distances y: {distances_y}')
 
     # if the distance is larger than 2 pixel, set the offset to the median
     offset_array = offset_array[np.logical_and(
@@ -131,7 +125,6 @@ def align_image(image_in: np.ndarray,
         distances_y < 2)]
 
     offset = np.mean(offset_array, axis=0)
-    print(f'Final Offset: {offset}')
     #offset /= float(num_references)
 
     if resize is not None:
